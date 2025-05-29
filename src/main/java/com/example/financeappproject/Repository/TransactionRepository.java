2b549188840d4db36856a141ccf96b600144d77c
bertilmoller <eaabhmn@students.eaaa.dk>
2025-05-29 10:10:34 +0200

    Opdateret Transactionrepository
    Opdateret klassen så den kunne håndtere at der skulle laves et summary endpoint.

--- src/main/java/com/example/financeappproject/Repository/TransactionRepository.java
+++ src/main/java/com/example/financeappproject/Repository/TransactionRepository.java
@@ -2,6 +2,8 @@ package com.example.financeappproject.Repository;
 
 import com.example.financeappproject.Model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -9,6 +11,8 @@ import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
-    //Spring læser metodenavn og oversætter det til SQL-query
     List<Transaction> findByDateBetweenAndCategory_Name(LocalDate startDate, LocalDate endDate, String categoryName);
+
+    @Query("SELECT SUM(t.amount) FROM Transaction t WHERE t.type = :type")
+    Double sumAmountByType(@Param("type") String type);
 }
